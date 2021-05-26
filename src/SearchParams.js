94fc1621a98bd5f06918bd52c291ea78eef32aa8
Yahya Jideh <yahya.jideh@appiancorp.com>
2021-05-26 10:09:12 +1000

    Started fetch api to request pets

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Pet from "./Pet";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
@@ -6,7 +7,18 @@ const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
+  const [pet, setPet] = useState([]);
   const breeds = [""];
+
+  useEffect(() => {
+    requestPets();
+  });
+
+  async function requestPets() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+  }
 
   return (
     <div className="search-params">
