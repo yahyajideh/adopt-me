8e3c10ea0f45311354b628e67914ca0ac1ac3f0d
yahyajideh <yahya.jideh@gmail.com>
2021-05-26 22:41:03 +1000

    :heavy_plus_sign: Completed pets fetch api, breedlist hook and handling user input.

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,28 +1,36 @@
 import { useState, useEffect } from "react";
+import useBreedList from "./useBreedList";
 import Pet from "./Pet";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
-  const [location, setLocation] = useState("Seattle, WA");
+  const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
-  const [pet, setPet] = useState([]);
-  const breeds = [""];
+  const [pets, setPets] = useState([]);
+  const [breeds] = useBreedList(animal);
 
   useEffect(() => {
     requestPets();
-  });
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   async function requestPets() {
     const res = await fetch(
       `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
     );
+    const json = await res.json();
+    setPets(json.pets);
   }
 
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location
           <input
@@ -40,7 +48,7 @@ const SearchParams = () => {
             onChange={(e) => setAnimal(e.target.value)}
             onBlur={(e) => setAnimal(e.target.value)}
           >
-            <option value="">Select option</option>
+            <option value=""></option>
             {ANIMALS.map((animal) => (
               <option key={animal} value={animal}>
                 {animal.charAt(0).toUpperCase() + animal.slice(1)}
@@ -56,7 +64,7 @@ const SearchParams = () => {
             onChange={(e) => setBreed(e.target.value)}
             onBlur={(e) => setBreed(e.target.value)}
           >
-            <option value="">Select option</option>
+            <option value=""></option>
             {breeds.map((breed) => (
               <option key={breed} value={breed}>
                 {breed.charAt(0).toUpperCase() + breed.slice(1)}
@@ -64,9 +72,16 @@ const SearchParams = () => {
             ))}
           </select>
         </label>
-
         <button>Submit</button>
       </form>
+      {pets.map((pet) => (
+        <Pet
+          key={pet.id}
+          name={pet.name}
+          animal={pet.animal}
+          breed={pet.breed}
+        />
+      ))}
     </div>
   );
 };
